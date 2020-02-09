0ca7212337916a5901f194fdc39babbec3da1c65
백승훈 <46189249+Walkers15@users.noreply.github.com>
2020-02-10 02:16:29 +0900

    라벨을 사용한 람다식의 반환

--- NoInlineLambdaReturn.kt
+++ NoInlineLambdaReturn.kt
@@ -15,7 +15,7 @@ fun retFunc(){
         if(result > 10) return@lit //return을 이용하면, 라벨을 사용한 블록의 끝부분으로 반환됨
         //즉, 흐름이 블록 바깥으로 나가서 다시 진행
         println("result : $result")
-    }
+    }//즉, 이 부분으로 빠져나와 다시 진행됨
     println("end of retFunc")
 }
 /*     Result     */
